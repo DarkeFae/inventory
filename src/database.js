db18d58b88e1f4bcce0441c9a144f0b0235c6a2c
DarkeFae <darkefae+github@gmail.com>
2024-06-13 23:23:08 +1200

    added a lot of functionality

--- src/database.js
+++ src/database.js
@@ -1,34 +1,118 @@
+const res = require("express/lib/response");
 const sqlite3 = require("sqlite3");
 
 function addItem(id, description, quantity, min, max, cost, rrp, wholesale) {
-    let db = new sqlite3.Database("db/inventory.db", (err) => {
-        if (err) {
-            console.error("Database opening error: ", err);
-            return;
-        }
-        console.log("Connected to the database.");
-    });
+  let db = new sqlite3.Database("db/inventory.db", (err) => {
+    if (err) {
+      console.error("Database opening error: ", err);
+      return;
+    }
+    console.log("Connected to the database.");
+  });
 
-    const sql = `INSERT INTO stock (id, description, quantity, min, max, cost, rrp, wholesale) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+  const sql = `INSERT INTO stock (id, description, quantity, min, max, cost, rrp, wholesale) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    db.run(sql, [id, description, quantity, min, max, cost, rrp, wholesale], (err) => {
-        if (err) {
-            console.error('Database insertion error: ', err);
-            return;
-        }
-        console.log('Row inserted.');
-    });
+  db.run(sql, [id, description, quantity, min, max, cost, rrp, wholesale], (err) => {
+    if (err) {
+      console.error('Database insertion error: ', err);
+      return;
+    }
+    console.log('Row inserted.');
+  });
 }
 
-function increaseItem() {}
+function increaseItem() { }
 
-function decreaseItem() {}
+function decreaseItem() { }
 
-function modifyItem() {}
+function modifyItem() { }
 
-function orderMore() {}
+function orderMore() {
+  let db = new sqlite3.Database("db/inventory.db", (err) => {
+    if (err) {
+      console.error("Database opening error: ", err);
+      return;
+    }
+    console.log("Connected to the database.");
+  });
 
-function queryItem() {}
+  const sql = `SELECT * FROM stock WHERE quantity < min AND needsOrdering = 1`;
+
+  return new Promise((resolve, reject) => {
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        console.error("Database query error: ", err);
+        reject(err);
+      }
+
+      if (rows && rows.length > 0) {
+        resolve(rows);
+      } else {
+        resolve([]);
+      }
+      });
+    db.close();
+  });
+}
+
+function order(id, needsOrdering) {
+  let db = new sqlite3.Database("db/inventory.db", (err) => {
+    if (err) {
+      console.error("Database opening error: ", err);
+      return;
+    }
+    console.log("Connected to the database.");
+  });
+
+  const sql = `UPDATE stock SET needsOrdering = ? WHERE id = ?`;
+
+  return new Promise((resolve, reject) => {
+    db.run(sql, [needsOrdering, id], (err) => {
+      if (err) {
+        console.error("Database update error: ", err);
+        reject(err);
+        return;
+      }
+      console.log(`Updated 'needsOrdering' for item with ID ${id}.`);
+      resolve();
+    });
+
+    db.close();
+  });
+}
+
+async function queryItem(id) {
+  let db = new sqlite3.Database("db/inventory.db", (err) => {
+    if (err) {
+      console.error("Database opening error: ", err);
+      return;
+    }
+    console.log("Connected to the database.");
+  });
+
+  const sql = `SELECT * FROM stock WHERE id = ?`;
+  let item = {}
+
+  return new Promise((resolve, reject) => {
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        console.error("Database query error: ", err);
+        reject(err);
+        return;
+      }
+
+      if (row) {
+        item.row = row;
+      } else {
+        console.log("Row not found.");
+      }
+
+      console.log(item);
+      resolve(item);
+      db.close();
+    });
+  });
+}
 
 function initTable() {
 
@@ -60,7 +144,7 @@ function initTable() {
               console.error("Table creation error: ", err);
             }
             console.log("Table created.");
-            
+
           }
         );
       }
@@ -77,4 +161,5 @@ module.exports = {
   orderMore,
   queryItem,
   initTable,
+  order,
 };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3');
-const { addItem, increaseItem, decreaseItem, modifyItem, orderMore, queryItem, initTable } = require('./database.js')
+const { addItem, increaseItem, decreaseItem, modifyItem, orderMore, queryItem, initTable, order } = require('./database.js')
 
 const db = new sqlite3.Database('db/inventory.db', (err) => {
     if (err) {
@@ -22,23 +22,66 @@ app.use(express.urlencoded({ extended: true }));
 const pages = ['addNew', 'query', 'modify', 'increase', 'decrease', 'orderMore'];
 
 app.get('/', (req, res) => {
-  res.render('layouts/layout.ejs', { page: "../home", navlinks: pages});
+    res.render('layouts/layout.ejs', { page: "../home", navlinks: pages });
 });
 
 /* Add routes for user to interact with. */
 pages.forEach(page => {
-    app.get(`/${page}`, (req, res) => {
-        res.render('layouts/layout', { page: `../${page}`, navlinks: pages });
-    });
+    if (page !== 'query' && page !== 'orderMore') {
+        app.get(`/${page}`, (req, res) => {
+            res.render('layouts/layout', { page: `../${page}`, navlinks: pages });
+        });
+    }
 });
 
 /* Add routes db interactions */
 app.post('/addNewItem', (req, res) => {
     const { id, description, quantity, min, max, cost, rrp, wholesale } = req.body;
-    addItem(id, description, quantity, min, max, cost, rrp, wholesale);
-    res.redirect(200 ,'/');
+    addItem(id.toLocaleUpperCase(), description, quantity, min, max, cost, rrp, wholesale);
+    res.redirect('/addNew');
+});
+
+app.get('/query', (req, res) => {
+    res.render('layouts/layout', { page: `../query`, navlinks: pages });
+});
+
+app.get('/orderMore', async (req, res) => {
+    try {
+        const items = await orderMore();
+        res.render('layouts/layout', { page: `../orderMore`, navlinks: pages, items });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred while querying items.' });
+    }
+});
+/* Database Queries and Modifications */
+app.get('/queryItem', async (req, res) => {
+    const id = req.query.id;
+    try {
+        const item = await queryItem(id.toLocaleUpperCase());
+        res.json(item);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred while querying the item.' });
+    }
+});
+
+app.post('/order', (req, res) => {
+    const { id, needsOrdering } = req.body;
+    order(id, needsOrdering)
+      .then(() => res.json({ success: true }))
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'An error occurred while updating the order status.' });
+      });
+  });
+
+app.post('/decreaseItems', async (req, res) => {
+    const items = req.body;
+    console.log(items);
+    res.status(200).type('json').json({ success: true });
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
